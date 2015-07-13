91fb2be2d773a9c5492c591e14a4865ae0c8a32a
Iain McDonald <iain.allan.mcdonald@googlemail.com>
2015-07-13 13:12:55 +0100

    adding scenario tag test module

---
+++ tests/unit/lib/utils/should-run-scenario.spec.js
@@ -0,0 +1,47 @@
+/* global describe, it, beforeEach */
+
+var assert = require('assert');
+
+describe('utils/should-run-feature', function () {
+
+    var testee,
+        tagRules = [
+            ['~@wip', '@awesome'],
+            ['@winner'],
+            ['@done', '@ace']
+        ];
+
+    beforeEach(function () {
+        testee = require('../../../../lib/utils/should-run-scenario');
+    });
+
+    it('should interpret ~ as meaning scenario should not have the tag', function () {
+        assert.equal(testee([['~@wip']], []), true);
+        assert.equal(testee([['~@wip']], ['@wip']), false);
+    });
+
+    it('should interpret no ~ as meaning scenario should have the tag', function () {
+        assert.equal(testee([['@wip']], []), false);
+        assert.equal(testee([['@wip']], ['@wip']), true);
+    });
+
+    it('should return true if scenario tags match one of the tag rules - I', function () {
+        var tags = ['@winner'];
+        assert.equal(testee(tagRules, tags), true);
+    });
+
+    it('should return true if scenario tags match one of the tag rules - II', function () {
+        var tags = ['@done', '@ace'];
+        assert.equal(testee(tagRules, tags), true);
+    });
+
+    it('should return false if scenario tags do not match one of the tag rules - I', function () {
+        var tags = ['@no-match'];
+        assert.equal(testee(tagRules, tags), false);
+    });
+
+    it('should return false if scenario tags do not match one of the tag rules - II', function () {
+        var tags = ['@wip', '@awesome'];
+        assert.equal(testee(tagRules, tags), false);
+    });
+});
