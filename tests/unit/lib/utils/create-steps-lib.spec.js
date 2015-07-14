a38959caa11cc81d4b802e4d053b033df0637729
Iain McDonald <iain.allan.mcdonald@googlemail.com>
2015-07-14 09:27:18 +0100

    moving logic out of template files into util functions

---
+++ tests/unit/lib/utils/create-steps-lib.spec.js
@@ -0,0 +1,110 @@
+/* global before, after, afterEach, describe, it, beforeEach */
+
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    rimraf = require('rimraf'),
+    copyFileWithReplacements = require('../../../../lib/utils/copy-file-with-replacements'),
+    proxyquire = require('proxyquire').noCallThru(),
+    sinon = require('sinon');
+
+describe('utils/create-steps-lib', function () {
+
+    var testee,
+        stubs = {},
+        stepsFilePattern = 'steps/**/*.steps.js';
+
+    beforeEach(function () {
+        stubs.fs = {
+            readFileSync: sinon.stub().returns('a feature'),
+            writeFileSync: sinon.stub()
+        };
+        stubs.defaultLibraryDefine = sinon.stub();
+        stubs.englishLibraryDefine = sinon.stub();
+        stubs.frenchLibraryDefine = sinon.stub();
+        stubs.yadda = {
+            localisation: {
+                English: {
+                    library: sinon.stub().returns({
+                        define: stubs.englishLibraryDefine
+                    })
+                },
+                French: {
+                    library: sinon.stub().returns({
+                        define: stubs.frenchLibraryDefine
+                    })
+                }
+            },
+            Library: sinon.stub().returns({
+                define: stubs.defaultLibraryDefine
+            }),
+            Yadda: sinon.spy(function () {
+                this.title = 'Yadda Instance with steps library';
+            })
+        };
+
+        var files = ['steps.js', 'path/to/steps.js', 'steppy.js'];
+
+        stubs['glob-all'] = {
+            sync: sinon.stub().returns(files)
+        };
+        files.forEach(function (file) {
+            stubs[path.resolve(process.cwd(), file)] = sinon.stub();
+        });
+        testee = proxyquire('../../../../lib/utils/create-steps-lib', stubs);
+    });
+
+
+    describe('localisation', function () {
+
+        it('should use default library if no localisation given', function () {
+            testee(stepsFilePattern, null);
+            assert.equal(stubs.yadda.Library.callCount, 1);
+        });
+
+        it('should use localisation library if localisation given - I', function () {
+            testee(stepsFilePattern, 'English');
+            assert.equal(stubs.yadda.localisation.English.library.callCount, 1);
+        });
+
+        it('should use localisation library if localisation given - II', function () {
+            testee(stepsFilePattern, 'French');
+            assert.equal(stubs.yadda.localisation.French.library.callCount, 1);
+        });
+
+    });
+
+    describe('other actions', function () {
+
+        var returnVal;
+        beforeEach(function () {
+            returnVal = testee(stepsFilePattern, null);
+        });
+
+        it('should define a close browser step on the library', function () {
+            assert.equal(stubs.defaultLibraryDefine.callCount, 1);
+            assert.equal(stubs.defaultLibraryDefine.args[0][0], 'close_browser');
+            var callback = stubs.defaultLibraryDefine.args[0][1],
+                browser = {
+                    end: sinon.stub()
+                };
+            callback.call({browser: browser});
+            assert.equal(browser.end.callCount, 1);
+        });
+
+        it('should call each step file passing in the library', function () {
+            stubs['glob-all'].sync().forEach(function (file) {
+                assert.equal(stubs[path.resolve(process.cwd(), file)].callCount, 1);
+                assert.equal(stubs[path.resolve(process.cwd(), file)].args[0][0].define, stubs.defaultLibraryDefine);
+            });
+        });
+
+        it('should create new Yadda instance using library and return it', function () {
+            assert.equal(stubs.yadda.Yadda.callCount, 1);
+            assert.equal(stubs.yadda.Yadda.args[0][0].define, stubs.defaultLibraryDefine);
+            assert.equal(returnVal.title, 'Yadda Instance with steps library');
+        });
+    });
+
+
+});
---
+++ tests/unit/lib/utils/decode-feature.spec.js
@@ -0,0 +1,115 @@
+/* global before, after, describe, it, beforeEach */
+
+var assert = require('assert'),
+    fs = require('fs'),
+    path = require('path'),
+    rimraf = require('rimraf'),
+    copyFileWithReplacements = require('../../../../lib/utils/copy-file-with-replacements'),
+    proxyquire = require('proxyquire').noCallThru(),
+    sinon = require('sinon');
+
+describe('utils/decode-feature', function () {
+
+    var testee,
+        returnVal,
+        stubs = {},
+        featureFilePath = 'dummy/path/to/feature.feature',
+        tagRules = [
+            ['~@1'],
+            ['@2', '@3'],
+            ['@4']
+        ];
+
+    beforeEach(function () {
+        stubs.fs = {
+            readFileSync: sinon.stub().returns('a feature'),
+            writeFileSync: sinon.stub()
+        };
+        stubs['/sandbox/steps-lib'] = {
+            yadda: sinon.stub()
+        };
+        stubs['../paths'] = {
+            NY_PATH: '/'
+        };
+        stubs['/utils/should-run-scenario'] = sinon.stub();
+        stubs.featureParserParse = sinon.stub().returns({
+            scenarios: Array.apply(null, {length: 6}).map(function (scenario, idx) {
+                var annotations = {};
+                annotations[idx] = true;
+                annotations[(idx + 1)] = true;
+                return {
+                    title: 'scenario' + idx,
+                    annotations: annotations,
+                    steps: Array.apply(null, {length: idx+1}).map(function (_, idx) {
+                        return idx;
+                    })
+                };
+            })
+        });
+        stubs.yadda = {
+            parsers: {
+                FeatureParser: function () {
+                    return {
+                        parse: stubs.featureParserParse
+                    };
+                }
+            }
+        };
+        testee = proxyquire('../../../../lib/utils/decode-feature', stubs);
+        returnVal = testee(featureFilePath, tagRules);
+    });
+
+    it('should read feature file', function () {
+        assert.equal(stubs.fs.readFileSync.callCount, 1);
+        assert.equal(stubs.fs.readFileSync.args[0][0], featureFilePath);
+        assert.equal(stubs.fs.readFileSync.args[0][1], 'UTF-8');
+    });
+
+    it('should parse file', function () {
+        assert.equal(stubs.featureParserParse.callCount, 1);
+        assert.equal(stubs.featureParserParse.args[0][0], stubs.fs.readFileSync());
+    });
+
+    it('should export function for each scenario', function () {
+        var scenarios = stubs.featureParserParse().scenarios;
+        scenarios.forEach(function (scenario) {
+            assert.equal(typeof returnVal[scenario.title], 'function');
+        });
+    });
+
+    it('should export function that runs scenario if it passes the tag test', function () {
+        var scenario = stubs.featureParserParse().scenarios[0],
+            scenarioFunc = returnVal[scenario.title];
+
+        stubs['/utils/should-run-scenario'].returns(true);
+        scenarioFunc('browser');
+        assert.equal(stubs['/utils/should-run-scenario'].callCount, 1);
+        assert.deepEqual(stubs['/utils/should-run-scenario'].args[0][0], tagRules);
+        assert.deepEqual(stubs['/utils/should-run-scenario'].args[0][1], ['@0', '@1']);
+        assert.equal(stubs['/sandbox/steps-lib'].yadda.callCount, 1);
+        assert.equal(stubs['/sandbox/steps-lib'].yadda.args[0][0], scenario.steps);
+        assert.equal(stubs['/sandbox/steps-lib'].yadda.args[0][1].browser, 'browser');
+    });
+
+    it('should export function that logs reason for not running the scenario if it does not pass the tag test', function () {
+        var scenario = stubs.featureParserParse().scenarios[1],
+            scenarioFunc = returnVal[scenario.title],
+            consoleLogSpy = sinon.spy(console, 'log'),
+            browser = {
+                end: sinon.stub()
+            };
+
+        stubs['/utils/should-run-scenario'].returns(false);
+        scenarioFunc(browser);
+        assert.equal(stubs['/utils/should-run-scenario'].callCount, 1);
+        assert.deepEqual(stubs['/utils/should-run-scenario'].args[0][0], tagRules);
+        assert.deepEqual(stubs['/utils/should-run-scenario'].args[0][1], ['@1', '@2']);
+        assert.equal(consoleLogSpy.callCount, 1);
+        assert.equal(consoleLogSpy.args[0][0], 'Not running scenario due to tag rules supplied');
+        assert.equal(browser.end.callCount, 1);
+        consoleLogSpy.restore();
+    });
+
+
+
+});
